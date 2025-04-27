9ff7ec49e2f7dbef88956e5f34ff77912d0ec93e
lukaklauzer <luka.klauzer9@gmail.com>
2025-04-27 21:42:46 +0200

    Update DI extension in Infrastructure

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Domain.Entities;
+using Core.Interfaces.Persistence.GenericRepository;
+using Core.Interfaces.Persistence.SpecificRepository;
+using Infrastructure.Persistance.ConcreteRepositories;
+using Infrastructure.Persistance.GenericRepository;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,6 +21,14 @@ namespace Infrastructure
           config.GetConnectionString("DefaultConnection"),
           b => b.MigrationsAssembly("Web.Api")));
 
+      services.AddScoped<IRepository<Customer>, Repository<Customer>>();
+      services.AddScoped<IRepository<Vehicle>, Repository<Vehicle>>();
+      services.AddScoped<IRepository<Rental>, Repository<Rental>>();
+
+      services.AddScoped<IVehicleRepository, VehicleRepository>();
+      services.AddScoped<ITelemetryRepository, TelemetryRepository>();
+      services.AddScoped<ICustomerRepository, CustomerRepository>();
+      services.AddScoped<IRentalRepository, RentalRepository>();
 
       return services;
     }
