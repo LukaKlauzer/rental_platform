96574a01dccd755f8609a5ae81ea1af9dfe6ac5f
lukaklauzer <luka.klauzer9@gmail.com>
2025-04-27 17:30:36 +0200

    AddInfrastructureServices with configuration from builder

--- rental_platform/Program.cs
+++ rental_platform/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddInfrastructureServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
