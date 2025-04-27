f8d45a1a71e5fe5305b8f249f80d0abc6d90b101
lukaklauzer <luka.klauzer9@gmail.com>
2025-04-27 21:41:02 +0200

    Add interfaces for loading data from CSV files

---
+++ Core/Interfaces/Data/ITelemetryDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using Core.Domain.Entities;
+using Core.Result;
+
+namespace Core.Interfaces.Data
+{
+  public interface ITelemetryDataProvider
+  {
+    Task<Result<IEnumerable<Telemetry>>> GetTelemetryDataAsync(CancellationToken cancellationToken = default);
+  }
+}
---
+++ Core/Interfaces/Data/IVehicleDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using Core.Domain.Entities;
+using Core.Result;
+
+namespace Core.Interfaces.Data
+{
+  public interface IVehicleDataProvider
+  {
+    Task<Result<IEnumerable<Telemetry>>> GetVehicleDataAsync(CancellationToken cancellationToken = default);
+  }
+}
