9a2cea7bd662de2940974206b0b4640c5c265a90
lukaklauzer <luka.klauzer9@gmail.com>
2025-04-29 17:40:41 +0200

    ): mark StartDate and EndDate as nullable

--- Core/DTOs/Rental/RentalUpdateDTO.cs
+++ Core/DTOs/Rental/RentalUpdateDTO.cs
@@ -3,7 +3,7 @@
   public class RentalUpdateDTO
   {
     public int Id { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
   }
 }
