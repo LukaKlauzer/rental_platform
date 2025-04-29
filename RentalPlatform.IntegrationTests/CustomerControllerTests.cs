25e8e409568051a9a2d598a1c68e93bf78d4a5e5
lukaklauzer <luka.klauzer9@gmail.com>
2025-04-29 17:49:38 +0200

    set up unit and integration test projects and add first tests

---
+++ RentalPlatform.IntegrationTests/CustomerControllerTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Net.Http.Json;
+using Core.DTOs.Customer;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using rental_platform.Controllers;
+
+namespace RentalPlatform.IntegrationTests
+{
+  public class CustomerControllerTests :
+    IClassFixture<WebApplicationFactory<CustomerController>>,
+    IAsyncLifetime
+  {
+    private readonly WebApplicationFactory<CustomerController> _webApplicationFactory;
+
+    private List<int> _customerIds;
+    public CustomerControllerTests(WebApplicationFactory<CustomerController> webApplicationFactory)
+    {
+      _webApplicationFactory = webApplicationFactory;
+
+      _customerIds = new List<int>();
+    }
+
+
+
+    [Fact]
+    public async Task GivenValidCustomerDto_CreatesCustomer()
+    {
+      // Arange
+      var httpClient = _webApplicationFactory.CreateClient();
+      var customerCreateDto = new CustomerCreateDTO() { Name = "Test user 1" };
+
+      // Act
+      var response = await httpClient.PostAsJsonAsync("api/customer", customerCreateDto);
+      var createdCustomer = await response.Content.ReadFromJsonAsync<CustomerReturnDTO>();
+
+      // Assert
+      response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+      createdCustomer.Should().NotBeNull();
+      createdCustomer.Name.Should().Be(customerCreateDto.Name);
+
+      _customerIds.Add(createdCustomer.Id);
+    }
+
+    [Fact]
+    public async Task CreateCustomer_RetrieveCustomerById() 
+    {
+      // Arange
+      var httpClient = _webApplicationFactory.CreateClient();
+      var customerCreateDto = new CustomerCreateDTO() { Name = "Test user 2"};
+
+      // Act
+      var responce = await httpClient.PostAsJsonAsync("api/customer", customerCreateDto);
+      var createdCustomer = await responce.Content.ReadFromJsonAsync<CustomerReturnDTO>();
+      createdCustomer.Should().NotBeNull();
+
+      // Assert
+      var getCustomer = await httpClient.GetAsync($"api/customer/{createdCustomer.Id}");
+      getCustomer.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+      var retreaveCustomer = await getCustomer.Content.ReadFromJsonAsync<CustomerReturnDTO>();
+      retreaveCustomer.Should().NotBeNull();
+      retreaveCustomer.Name.Should().Be(customerCreateDto.Name);
+      
+      _customerIds.Add(createdCustomer.Id);
+    }
+
+
+    public Task InitializeAsync()
+    {
+      return Task.CompletedTask;
+    }
+
+    public async Task DisposeAsync()
+    {
+      var httpClient = _webApplicationFactory?.CreateClient();
+      foreach (var id in _customerIds)
+        await httpClient.DeleteAsync($"api/customer/{id}");
+
+    }
+  }
+}
---
+++ RentalPlatform.Tests/Application/Customer/CustomerServiceTests.cs
@@ -0,0 +1,176 @@
+﻿using Application.Services;
+using Core.Domain.Entities;
+using Core.DTOs.Customer;
+using Core.Enums;
+using Core.Interfaces.Persistence.SpecificRepository;
+using Core.Interfaces.Services;
+using Core.Result;
+using Moq;
+using CustomerEntity = Core.Domain.Entities.Customer;
+
+namespace RentalPlatform.UnitTests.Application.Customer
+{
+  public class CustomerServiceTests
+  {
+    private readonly Mock<ICustomerRepository> _mockCustomerRepository;
+    private readonly Mock<IRentalRepository> _mockRentalRepository;
+    private readonly Mock<IVehicleRepository> _mockVehicleRepository;
+    private readonly ICustomerService _customerService;
+
+    public CustomerServiceTests()
+    {
+      _mockCustomerRepository = new Mock<ICustomerRepository>();
+      _mockRentalRepository = new Mock<IRentalRepository>();
+      _mockVehicleRepository = new Mock<IVehicleRepository>();
+
+      _customerService = new CustomerSevice(
+          _mockCustomerRepository.Object,
+          _mockRentalRepository.Object,
+          _mockVehicleRepository.Object);
+    }
+    [Fact]
+    public async Task Create_WithValidData_ShouldReturnSuccess()
+    {
+      // Arange
+      var createCustomerDto = new CustomerCreateDTO() { Name = "Test customer 1" };
+      var customer = new CustomerEntity { ID = 1, Name = "Test Customer 1" };
+
+      _mockCustomerRepository.Setup(repo => repo
+        .Create(It.IsAny<CustomerEntity>(), default))
+        .ReturnsAsync(Result<CustomerEntity>.Success(customer));
+
+      // Act
+      var result = await _customerService.Create(createCustomerDto);
+
+      // Assert
+      Assert.True(result.IsSuccess);
+      Assert.Equal("Test Customer 1", result.Value.Name);
+    }
+
+
+    [Fact]
+    public async Task CreateWithNotValidData_ShouldReturnError()
+    {
+      // Arange 
+      var createCustomerDto = new CustomerCreateDTO() { Name = "" };
+
+      // Act
+      var result = await _customerService.Create(createCustomerDto);
+
+      // Assert
+      Assert.True(result.IsFailure);
+      Assert.Equal(ErrorType.ValidationError, result.Error.ErrorType);
+    }
+
+    [Fact]
+    public async Task CreateWithNullDto_ShouldReturnError()
+    {
+      // Arange 
+      CustomerCreateDTO createCustomerDto = null;
+
+      // Act
+      var result = await _customerService.Create(createCustomerDto);
+
+      // Assert
+      Assert.True(result.IsFailure);
+      Assert.Equal(ErrorType.NullReference, result.Error.ErrorType);
+    }
+
+    [Fact]
+    public async Task GetById_CustomerNotFound_ReturnsError()
+    {
+      // Arrange
+      var customerId = 1;
+      _mockCustomerRepository.Setup(repo => repo.GetById(customerId, default))
+        .ReturnsAsync(Result<CustomerEntity>.Failure(Error.NotFound("Customer with id '{customerId}'")));
+
+      // Act
+      var result = await _customerService.GetById(customerId);
+
+      // Assert
+      Assert.True(result.IsFailure);
+      Assert.Equal(ErrorType.NotFound, result.Error.ErrorType);
+    }
+
+    [Fact]
+    public async Task GetById_CustomerWithCompleatedRentals_CalculatesTortalsCorrectly()
+    {
+      // Arrange
+      var customerId = 1;
+      string vehicle1Vin = "VIN2";
+      string vehicle2Vin = "VIN1";
+
+      var customer = new CustomerEntity() { ID = customerId, Name = "Test customer 1" };
+
+      var rentals = new List<Rental>
+      {
+        new Rental
+        {
+          ID = 1,
+          CustomerId = customerId,
+          VehicleId = vehicle1Vin,
+
+          StartDate = new DateTime(2025, 1, 1),
+          EndDate = new DateTime(2025, 1,3),
+
+          OdometerStart = 0,
+          OdometerEnd = 500,
+
+          BatterySOCStart = 80,
+          BatterySOCEnd=30
+        }
+      };
+      var vehacleVins = new List<string> { vehicle1Vin };
+      var vehicles = new List<Vehicle>
+      {
+        new Vehicle
+        {
+          Vin = vehicle1Vin,
+          Make = "Mazdda",
+          Model = "Miata",
+          Year = 2019,
+          PricePerDayInEuro = 0.5f,
+          PricePerKmInEuro = 100f
+        }
+    };
+      var vehicleDict = vehicles.ToDictionary(v => v.Vin);
+
+      _mockCustomerRepository.Setup(repo => repo.GetById(customerId, default)).ReturnsAsync(Result<CustomerEntity>.Success(customer));
+      _mockRentalRepository.Setup(repo => repo.GetByCustomerId(customerId, default)).ReturnsAsync(Result<IEnumerable<Rental>>.Success(rentals));
+      _mockVehicleRepository.Setup(repo => repo.GetByVins(vehacleVins, default)).ReturnsAsync(Result<IEnumerable<Vehicle>>.Success(vehicles));
+
+      // Act
+      var result = await _customerService.GetById(customerId);
+
+      /**
+        `total_kilometers_per_rental × price_per_km_in_euro`  
+        `+ number_of_rental_days × price_per_day_in_euro`  
+        `+ max(0, -battery_delta_per_rental) × 0.2€`
+      **/
+      var rentalStats = rentals.Select(rental =>
+      {
+        if (!vehicleDict.TryGetValue(rental.VehicleId, out var vehicle))
+          return null;
+        float distance = rental.OdometerEnd!.Value - rental.OdometerStart;
+        float days = Math.Max(1, (int)Math.Ceiling((rental.EndDate - rental.StartDate).TotalDays));
+        float batteryDelta = rental.BatterySOCEnd!.Value - rental.BatterySOCStart;
+        return new
+        {
+          Distance = distance,
+          Days = days,
+          Cost = distance * vehicle.PricePerKmInEuro + days * vehicle.PricePerDayInEuro + Math.Max(0, -batteryDelta) * 0.2f,
+          Vehicle = vehicle
+        };
+      }
+
+        );
+      var distance = rentalStats.Sum(s => s!.Distance);
+      var price = rentalStats.Sum(s => s!.Cost);
+
+      // Assert
+      Assert.True(result.IsSuccess);
+      Assert.Equal(distance, result.Value.TotalDistanceDriven);
+      Assert.Equal(price, result.Value.TotalPrice);
+    }
+  }
+}
