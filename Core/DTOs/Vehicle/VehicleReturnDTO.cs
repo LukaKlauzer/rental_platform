5ea19c8ab7d32aba9d6dc52c03aedc03acdc6d9f
lukaklauzer <luka.klauzer9@gmail.com>
2025-04-29 06:30:40 +0200

    change DateOnly to int in return DTO for year

--- Core/DTOs/Vehicle/VehicleReturnDTO.cs
+++ Core/DTOs/Vehicle/VehicleReturnDTO.cs
@@ -5,7 +5,7 @@
     public string Vin { get; set; } = string.Empty;
     public string Make { get; set; } = string.Empty;
     public string Model { get; set; } = string.Empty;
-    public DateOnly Year { get; set; }
+    public int Year { get; set; }
     public float PricePerKmInEuro { get; set; }
     public float PricePerDayInEuro { get; set; }
   }
