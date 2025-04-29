2ac4230c8c42688351ea5a3537c8e91316326830
lukaklauzer <luka.klauzer9@gmail.com>
2025-04-29 06:29:48 +0200

    add Telemetry DTO

---
+++ Core/DTOs/Telemetry/TelemetryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.DTOs.Telemetry
+{
+  public class TelemetryDTO
+  {
+    public string VehicleId { get; set; } = string.Empty;
+    public int Name { get; set; }
+    public int Timestamp { get; set; }
+  }
+}
