55c764876f47dfefdb463342e1e6a131fa2504db
lukaklauzer <luka.klauzer9@gmail.com>
2025-04-29 17:49:02 +0200

    add Swagger UI support for OpenAPI

--- rental_platform/Program.cs
+++ rental_platform/Program.cs
@@ -18,7 +18,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  app.MapOpenApi();
+  app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", ""));
 }
 
 app.UseHttpsRedirection();
