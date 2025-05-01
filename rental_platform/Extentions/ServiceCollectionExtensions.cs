3884b6b3e68a0ea42f6c89ab075f9a7a8cef80ba
lukaklauzer <luka.klauzer9@gmail.com>
2025-05-01 16:18:58 +0200

    add token support to Swagger

---
+++ rental_platform/Extentions/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+namespace rental_platform.Extentions
+{
+  public static class ServiceCollectionExtensions
+  {
+    internal static IServiceCollection AddSwaggerGenWithAuth(this IServiceCollection services)
+    {
+      services.AddSwaggerGen(options =>
+      {
+        var securityScheme = new OpenApiSecurityScheme
+        {
+          Name = "JWT Authentification",
+          Description = "Enter JWT token",
+          In = ParameterLocation.Header,
+          Type = SecuritySchemeType.Http,
+          Scheme = "bearer",//JwtBearerDefaults.AuthenticationScheme,
+          BearerFormat = JwtBearerDefaults.AuthenticationScheme,
+
+        };
+        options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
+
+        var securityRequierment = new OpenApiSecurityRequirement
+        {
+          {
+            new OpenApiSecurityScheme{
+              Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = JwtBearerDefaults.AuthenticationScheme
+              }
+            },[]
+          }
+        };
+        options.AddSecurityRequirement(securityRequierment);
+      });
+
+      return services;
+    }
+
+  }
+}
