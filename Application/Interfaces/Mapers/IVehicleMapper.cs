f0ea8eb9485735ec09b8ee45041b761ae812d915
lukaklauzer <luka.klauzer9@gmail.com>
2025-05-11 07:47:12 +0200

    add mappers for data transformation

---
+++ Application/Interfaces/Mapers/IVehicleMapper.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs.Vehicle;
+using Core.Domain.Entities;
+using Core.Result;
+
+namespace Application.Interfaces.Mapers
+{
+  public interface IVehicleMapper
+  {
+    public Result<Vehicle> ToEntity(VehicleCreateDto dto);
+    public Result<VehicleReturnDto> ToReturnDto(Vehicle entity);
+    public Result<VehicleReturnSingleDto> ToReturnSingleDto(Vehicle entity, float totalDistanceDriven = 0f, int totalRentalCount = 0, float totalRentalIncome = 0f);
+    public Result<List<VehicleReturnDto>> ToReturnDtoList(IEnumerable<Vehicle> entities);
+  }
+}
---
+++ Application/Mapers/RentalMapper.cs
@@ -0,0 +1,87 @@
+﻿using Application.DTOs.Rental;
+using Application.Interfaces.Mapers;
+using Core.Domain.Entities;
+using Core.Result;
+
+namespace Application.Mapers
+{
+  internal class RentalMapper : IRentalMapper
+  {
+    public Result<RentalReturnDto> ToReturnDto(Rental entity)
+    {
+      if (entity is null)
+        return Result<RentalReturnDto>.Failure(Error.NullReferenceError("Rental entity cannot be null"));
+
+      var dto = new RentalReturnDto(
+        Id: entity.ID,
+        StartDate: entity.StartDate,
+        EndDate: entity.EndDate,
+        RentalStatus: entity.RentalStatus,
+        VehicleId: entity.VehicleId,
+        CustomerId: entity.CustomerId
+      );
+
+      return Result<RentalReturnDto>.Success(dto);
+    }
+
+    public Result<RentalReturnSingleDto> ToReturnSingleDto(
+      Rental entity,
+      float? distanceTraveled)
+    {
+      if (entity is null)
+        return Result<RentalReturnSingleDto>.Failure(Error.NullReferenceError("Rental entity cannot be null for single DTO mapping"));
+
+
+      var dto = new RentalReturnSingleDto(
+        Id: entity.ID,
+        StartDate: entity.StartDate,
+        EndDate: entity.EndDate,
+        RentalStatus: entity.RentalStatus,
+        VehicleId: entity.VehicleId,
+        CustomerId: entity.CustomerId,
+        DistanceTraveled: distanceTraveled,
+        BatterySOCSAtStart: entity.BatterySOCStart,
+        BatterySOCAtEnd: entity.BatterySOCEnd ?? 0f
+      );
+
+      return Result<RentalReturnSingleDto>.Success(dto);
+    }
+
+    public Result<List<RentalReturnDto>> ToReturnDtoList(IEnumerable<Rental> entities)
+    {
+      if (entities is null)
+        return Result<List<RentalReturnDto>>.Failure(Error.NullReferenceError("Rental collection can not be null"));
+
+      var dtos = new List<RentalReturnDto>();
+
+      foreach (var entity in entities)
+      {
+        var dtoResult = ToReturnDto(entity);
+        if (dtoResult.IsFailure)
+          return Result<List<RentalReturnDto>>.Failure(dtoResult.Error);
+
+        dtos.Add(dtoResult.Value);
+      }
+
+      return Result<List<RentalReturnDto>>.Success(dtos);
+    }
+
+    public Result<Rental> ToEntity(
+      RentalCreateDto dto,
+      float odometorStart,
+      float batterySOCStart,
+      float? odometorEnd = null,
+      float? batterySOCEnd = null)
+    {
+      return Rental.Create(
+        startDate: dto.StartDate,
+        endDate: dto.EndDate,
+        odometerStart: odometorStart,
+        batterySOCStart: batterySOCStart,
+        vehicleId: dto.VehicleId,
+        customerId: dto.CustomerId,
+        odometerEnd: odometorEnd,
+        batterySOCEnd: batterySOCEnd);
+    }
+  }
+}
